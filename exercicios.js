ae8a39d4d17554ce7e1febfee3f5f29957a230c5
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-15 17:22:12 -0300

    operadores de comparação && operadores lógicos!
    && -> and -> E
    || -> or -> ou
    ! -> not -> nao

--- exercicios.js
+++ exercicios.js
@@ -1,28 +1,6 @@
-function meuEscopo (){
-    const form = document.querySelector('.form');
-    const resultado = document.querySelector('.resultado');
-
-    const pessoas =[];
-
-    function recebeEventoForm(evento) {
-        evento.preventDefault();
-        const nome = form.querySelector('.nome');
-        const sobrenome = form.querySelector('.sobrenome');
-        const peso = form.querySelector('.peso');
-        const altura = form.querySelector('.altura');
-
-        pessoas.push({
-            nome: nome.value,
-            sobrenome: sobrenome.value,
-            peso: peso.value,
-            altura: altura.value,
-
-        });
-        console.log(pessoas);
-        
-        resultado.innerHTML += `<p>seu nome completo é ${nome.value} ${sobrenome.value} O Sr. pesa ${peso.value}kg. e tem ${altura.value} de altura</p>`
-    }
-
-    form.addEventListener('submit', recebeEventoForm);
-}
-meuEscopo();
+/* 
+operadores logicos
+&& -> and -> E -> todas as expressoes precicam ser verdadeiras
+|| -> or -> ou -> uma expressoes precicam ser verdadeira, para ser true..
+! -> not -> nao -> negação, inverção de valor
+*/
