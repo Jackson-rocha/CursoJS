65bbe68e7c326e2c13ed061b4825a589968ef7d4
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-16 01:10:30 -0300

    Exercícios...

--- exercicios.js
+++ exercicios.js
@@ -1,6 +1,1 @@
-/* 
-operadores logicos
-&& -> and -> E -> todas as expressoes precicam ser verdadeiras
-|| -> or -> ou -> uma expressoes precicam ser verdadeira, para ser true..
-! -> not -> nao -> negação, inverção de valor
-*/
+alert('helloword')
--- index.js
+++ index.js
@@ -347,8 +347,28 @@ operadores logicos
 ! -> not -> nao -> negação, inverção de valor
 */
 
+/*
+   if e else ifs elses
+ 
+   if = pode ser usado sozinho
+   sempre que ultilizo a palavra else, preciso de um if antes
+   Eu posso ter varios elses ifs na checagem 
 
 
+const hora = 45;
+
+if (hora >= 6 && hora <= 12){
+    console.log('Bom dia!');
+}else if (hora >= 12 && hora <= 18) {
+    console.log('Boa tarde!');
+}else if (hora >= 18 && hora <= 24) {
+    console.log('Boa noite!');
+}else if (hora >= 0 && hora <= 6) {
+    console.log('Boa Madrugada!');
+}else{
+    console.log('não encontramos a Hora')
+}
+*/
 
 
 
