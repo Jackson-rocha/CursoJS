74e6450fcfa8ef1dbc8cbbb72599fce4f6a9f13f
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-18 20:17:32 -0300

    Calculando IMC

--- exercicios.js
+++ exercicios.js
@@ -4,9 +4,44 @@ const form = document.querySelector('#form');
 
 form.addEventListener ('submit' , function (e) {
     e.preventDefault();
-    console.log('Evento previnido.');
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    
+    if(!peso){
+        setResultado('peso inválido', false);
+        return;
+    }
+    if(!altura){
+        setResultado('altura invalida', false);
+        return;
+    }
+    const imc = getImc(peso, altura);
+
+    console.log(imcmc)
+    
+        
 });
 
-function setResultado(){
 
+function getImc(peso, altura){
+        const imc = peso / altura ** 2;
+        return imc.toFixed(2);
+    };
+
+
+function criaP(){
+    const p = document.createElement('p');
+    return p;
+};
+
+function setResultado(msg,){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = msg;
+
+    const p = criaP
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 };
