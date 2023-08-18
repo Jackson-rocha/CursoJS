ddc4ee8a6acbbebc803aaf6527a95d8f9c6a28ea
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-17 23:55:45 -0300

    submit

--- exercicios.js
+++ exercicios.js
@@ -1,1 +1,8 @@
-alert('helloword')
+//capturar evento de submit do formulario
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (e){
+    e.preventDefault();
+    console.log('Evento previnido');
+});
