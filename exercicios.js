e00188c1a775777b4097ecf32d43768910fbb1ec
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-18 09:18:23 -0300

    Exercicios...

--- exercicios.js
+++ exercicios.js
@@ -2,7 +2,11 @@
 
 const form = document.querySelector('#form');
 
-form.addEventListener('submit', function (e){
+form.addEventListener ('submit' , function (e) {
     e.preventDefault();
-    console.log('Evento previnido');
+    console.log('Evento previnido.');
 });
+
+function setResultado(){
+
+};
