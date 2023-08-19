23dd3be6d307bba6ec8c8694fc1374473e738344
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-19 19:55:29 -0300

    // estruturas switch/case

--- exercicios.js
+++ exercicios.js
@@ -1,31 +1,34 @@
-// objeto date  
+// estruturas switch/case
 
-/* const data = new Date('2019-04 20:20:59');
-console.log('dia', data.getDate());
-console.log('mes', data.getMonth() + 1);//mes começa do zero 
-console.log('ano', data.getFullYear());
-console.log('hora', data.getHours());
-console.log('min', data.getMinutes());
-console.log('seg', data.getSeconds());
-console.log('ms', data.getMilliseconds());
-console.log('dia da semana', data.getDay());// 0 - domingo / 6- sabado
-console.log(data.toString()); 
+const data = new Date('1987-04-26 00:00:00');
+let diaSemana = data.getDay();
+diaSemana = 8;
+let diaSemanaTexto;
 
-function zeroAesquerda (num) {
-    return num >= 10 ? num : `0${num}`;
+switch(diaSemana){
+    case 0:
+        diaSemanaTexto ='domingo';
+        break;
+    case 1:
+        diaSemanaTexto ='segunda';
+        break;
+    case 2:
+        diaSemanaTexto ='terça';
+        break;
+    case 3:
+        diaSemanaTexto ='quarta';
+        break;
+    case 4:
+        diaSemanaTexto ='quinta';
+        break;
+    case 5:
+        diaSemanaTexto ='sexta';
+        break;
+    case 6:
+        diaSemanaTexto ='sabado';
+        break;
+    default:
+        diaSemanaTexto = 'dia invalido';
 }
 
-function formataData(data) {
-    const dia = zeroAesquerda(data.getDate());
-    const mes = zeroAesquerda(data.getMonth() + 1);
-    const ano = zeroAesquerda(data.getFullYear());
-    const hora = zeroAesquerda(data.getHours());
-    const min = zeroAesquerda(data.getMinutes());
-    const seg = zeroAesquerda(data.getSeconds());
-
-    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
-}
-
-const data = new Date();
-const dataBrasil = formataData(data);
-console.log(dataBrasil);*/
+console.log(diaSemana, diaSemanaTexto);
--- index.js
+++ index.js
@@ -484,6 +484,39 @@ const data = new Date();
 const dataBrasil = formataData(data);
 console.log(dataBrasil);*/
 
+// estruturas switch/case
 
+/* const data = new Date('1987-04-26 00:00:00');
+let diaSemana = data.getDay();
+diaSemana = 8;
+let diaSemanaTexto;
+
+switch(diaSemana){
+    case 0:
+        diaSemanaTexto ='domingo';
+        break;
+    case 1:
+        diaSemanaTexto ='segunda';
+        break;
+    case 2:
+        diaSemanaTexto ='terça';
+        break;
+    case 3:
+        diaSemanaTexto ='quarta';
+        break;
+    case 4:
+        diaSemanaTexto ='quinta';
+        break;
+    case 5:
+        diaSemanaTexto ='sexta';
+        break;
+    case 6:
+        diaSemanaTexto ='sabado';
+        break;
+    default:
+        diaSemanaTexto = 'dia invalido';
+}
+
+console.log(diaSemana, diaSemanaTexto);*/
 
 
