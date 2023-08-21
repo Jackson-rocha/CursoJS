00edda89cd9438b6e8f9648594c75a1c314f959b
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-20 21:45:14 -0300

    Atribuição via desestruturação
    ...rest
    ...spread

--- index.js
+++ index.js
@@ -629,4 +629,32 @@ const opcoes = {
 };
 
 h1.innerHTML = data.toLocaleDateString( {dateStyle: 'full', timeStyle: 'short'});
+
+
+
+// Atribuição via desestruturação
+
+let a = 'A';
+let b = 'B';
+let c = 'C';
+
+const letras = [b, c, a];
+[a, b, c] = letras;
+
+console.log(a, b, c);
+
+const numeros =[1, 2, 3, 4, 5, 6, 7, 8, 9];
+const [um, dois, tres, ...resto] =numeros;
+console.log(um, dois, tres);
+console.log(resto);
+
+
+// ...rest, ...spread
+//                   0           1           2
+//                0  1  2     0  1  2     0  1  2 
+const numeros =[ [1, 2, 3,], [4, 5, 6], [ 7, 8, 9] ];
+const [lista1, lista2, lista3] =numeros;
+console.log(lista3[0]);
+
+ 
 */
