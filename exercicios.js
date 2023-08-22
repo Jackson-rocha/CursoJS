f8c6b08f63be2be4ae6106ce6bade54cb6e890e0
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-21 23:59:16 -0300

    Exercicio com for

--- exercicios.js
+++ exercicios.js
@@ -1,7 +1,21 @@
-// estrutura de repetição
+//Exercicio com for
 
-const frutas = ['pera', 'uva', 'laranja'];
+const elementos = [
+    {tag: 'p', texto: 'qualquer texto que voce quiser'},
+    {tag: 'div', texto: 'frase 2'},
+    {tag: 'section', texto: 'frase 3'},
+    {tag: 'footer', texto: 'frase 4'},
+];
 
-for (let i = 0; i < frutas.length; i++) {/*+=10*/
-    console.log(i);
-};
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++){
+    let{tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerHTML = texto;
+    div.appendChild(tagCriada);
+    
+}
+
+container.appendChild(div);
--- index.js
+++ index.js
@@ -668,3 +668,25 @@ for (let i = 0; i < frutas.length; i++) { +=10
     console.log(i);
 };
 */
+//Exercicio com for
+/*
+const elementos = [
+    {tag: 'p', texto: 'qualquer texto que voce quiser'},
+    {tag: 'div', texto: 'frase 2'},
+    {tag: 'section', texto: 'frase 3'},
+    {tag: 'footer', texto: 'frase 4'},
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++){
+    let{tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerHTML = texto;
+    div.appendChild(tagCriada);
+    
+}
+
+container.appendChild(div);
+*/
