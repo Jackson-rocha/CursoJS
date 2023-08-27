50e547f5af812aa8057b69782fd27377ad07c5da
Jackson-rocha <jackson.ads.dev@gmail.com>
2023-08-26 21:46:01 -0300

    // returno de função

--- exercicios.js
+++ exercicios.js
@@ -1,11 +1,1 @@
-//parametros
-function conta(operador, acumulador, ...numeros){
-    for(let numero of numeros){
-        if(operador === '+') acumulador += numero;
-        if(operador === '-') acumulador -= numero;
-        if(operador === '*') acumulador *= numero;
-        if(operador === '/') acumulador /= numero;
-    }
-    console.log(acumulador);    
-}
-conta('*', 3, 3, 5);
+// escopo lexico
--- index.js
+++ index.js
@@ -1109,5 +1109,19 @@ function conta(operador, acumulador, ...numeros){
 }
 conta('*', 3, 3, 5);
 
+// returno de função
 
+function criaMultiplicador(multiplicador){
+    return function(n){
+        return n* multiplicador;
+    }
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
 */
